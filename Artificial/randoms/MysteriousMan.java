7d2c73a9ca89608889b1aaed4f20c74c558c1fcc
Calle2Pro <zerocoder@live.se>
2013-10-22 16:09:34 +0200

    Mysterious Man Random
    Solves it :)

---
+++ Artificial/randoms/MysteriousMan.java
@@ -0,0 +1,44 @@
+package dynamic.randoms;
+
+import org.shadowbot.bot.api.RandomEvent;
+import org.shadowbot.bot.api.methods.input.Mouse;
+import org.shadowbot.bot.api.methods.interactive.NPCs;
+import org.shadowbot.bot.api.util.Random;
+import org.shadowbot.bot.api.util.Time;
+import org.shadowbot.bot.api.wrapper.NPC;
+import org.shadowbot.bot.api.wrapper.Widget;
+import org.shadowbot.bot.api.wrapper.WidgetChild;
+
+/**
+ * User: Pen
+ */
+public class MysteriousMan extends RandomEvent {
+
+    @Override
+    public String getName() {
+        return "Mysterious Old Man";  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Calle";  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int solve() {
+        NPC dude = NPCs.getNearest(new int[]{4660});
+        if (dude.isOnScreen()) {
+            dude.interact("Talk to");
+            Time.sleep(Random.nextInt(100, 150));
+            // Mouse.move(100, 200); // Moves to Contiune Button
+            // Mouse.click(100, 200); // Clicks Continue?
+
+        }
+        return Random.nextInt(100, 150);  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean isActive() {
+        return true;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
